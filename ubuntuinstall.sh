e2c287490768a7e5d684a9921c8499bcdff93c21
Oliver Thomas <oliver.thomas@bristol.ac.uk>
2018-05-19 20:47:14 +0100

    changed to only one apt-get install step

--- ubuntuinstall.sh
+++ ubuntuinstall.sh
@@ -1,18 +1,8 @@
-#install python jupyter notebooks
-sudo apt-get install -y python-pip pandoc python-setuptools
-sudo pip install jupyter
-#run on new install
+sudo add-apt-repository ppa:symengine/ppa 
 sudo add-apt-repository ppa:webupd8team/indicator-kdeconnect 
-sudo apt-get install indicator-kdeconnect kdeconnect
-#Symengine
-sudo add-apt-repository ppa:symengine/ppa
 sudo apt-get update
-sudo apt-get install libsymengine-dev python-symengine  
-#Latex for vim and Linux
-sudo apt-get install texlive-full biber
-#Vim paste with "+p 
-sudo apt-get install vim-gtk
+sudo apt-get install -y python-pip pandoc python-setuptools indicator-kdeconnect kdeconnect libsymengine-dev python-symengine vim-gtk okular texlive-full biber 
+sudo pip install jupyter
+#Vim theme paste with "+p 
 sudo wget "https://raw.githubusercontent.com/TroyFletcher/vim-colors-synthwave/master/colors/synthwave.vim" -P ~/.vim/colors/
-#Okular & kde libs
-sudo apt-get install -y okular
 
