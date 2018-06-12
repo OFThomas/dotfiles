93ced0c6e2ccdb0d9f8724b53b039d9ccf924570
Oliver Thomas <oliver.thomas@bristol.ac.uk>
2018-06-12 20:16:30 +0100

    update ubuntu install script to python3

--- ubuntuinstall.sh
+++ ubuntuinstall.sh
@@ -1,8 +1,8 @@
-sudo add-apt-repository ppa:symengine/ppa 
-sudo add-apt-repository ppa:webupd8team/indicator-kdeconnect 
+#sudo add-apt-repository ppa:symengine/ppa 
+#sudo add-apt-repository ppa:webupd8team/indicator-kdeconnect 
 sudo apt-get update
-sudo apt-get install -y python-pip pandoc python-setuptools indicator-kdeconnect kdeconnect libsymengine-dev python-symengine vim-gtk okular texlive-full biber screenfetch
-sudo pip install jupyter
+sudo apt-get install -y python-pip pandoc python-setuptools indicator-kdeconnect kdeconnect libsymengine-dev python-symengine python3-numpy python3-matplotlib python3-scipy python3-nose ipython3 python3-pip python3-sympy vim-gtk okular texlive-full biber screenfetch
+#sudo pip install jupyter
 #Vim theme paste with "+p 
-sudo wget "https://raw.githubusercontent.com/TroyFletcher/vim-colors-synthwave/master/colors/synthwave.vim" -P ~/.vim/colors/
+#sudo wget "https://raw.githubusercontent.com/TroyFletcher/vim-colors-synthwave/master/colors/synthwave.vim" -P ~/.vim/colors/
 
