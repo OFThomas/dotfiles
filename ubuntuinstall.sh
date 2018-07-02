778b4f8454bb503948d36aab38c840cbc100a2d1
Oliver Thomas <oliver.thomas@bristol.ac.uk>
2018-07-02 18:53:27 +0100

    added in tkinter and projectq install

--- ubuntuinstall.sh
+++ ubuntuinstall.sh
@@ -1,7 +1,11 @@
 #sudo add-apt-repository ppa:symengine/ppa 
 #sudo add-apt-repository ppa:webupd8team/indicator-kdeconnect 
 sudo apt-get update
-sudo apt-get install -y python-pip pandoc python-setuptools indicator-kdeconnect kdeconnect libsymengine-dev python-symengine python3-numpy python3-matplotlib python3-scipy python3-nose ipython3 python3-pip python3-sympy vim-gtk okular texlive-full biber screenfetch
+sudo apt-get install -y build-essential
+
+sudo apt-get install -y  python3 python3-dev python3-setuptools python-pip pandoc python-setuptools indicator-kdeconnect kdeconnect libsymengine-dev python-symengine python3-numpy python3-matplotlib python3-scipy python3-nose ipython3 python3-pip python3-sympy python3-tk vim-gtk okular texlive-full biber screenfetch
+python -m pip install --user projectq
+sudo pip3 install --user projectq
 #sudo pip install jupyter
 #Vim theme paste with "+p 
 #sudo wget "https://raw.githubusercontent.com/TroyFletcher/vim-colors-synthwave/master/colors/synthwave.vim" -P ~/.vim/colors/
