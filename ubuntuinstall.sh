1d1fcbf697601266876addb7bd4d266d77cee7b1
Oliver <oliver.thomas404@gmail.com>
2018-07-02 20:22:53 +0100

    updated pip to use python3 -m pip

--- ubuntuinstall.sh
+++ ubuntuinstall.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get install -y build-essential
 
 sudo apt-get install -y  python3 python3-dev python3-setuptools python-pip pandoc python-setuptools indicator-kdeconnect kdeconnect libsymengine-dev python-symengine python3-numpy python3-matplotlib python3-scipy python3-nose ipython3 python3-pip python3-sympy python3-tk vim-gtk okular texlive-full biber screenfetch
-sudo -H pip3 install --user --upgrade projectq
+python3 -m pip install --user projectq
 #sudo pip install jupyter
 #Vim theme paste with "+p 
 #sudo wget "https://raw.githubusercontent.com/TroyFletcher/vim-colors-synthwave/master/colors/synthwave.vim" -P ~/.vim/colors/
