bad43b8d015e5264f999db2c24d82f0fb9367c7a
Oliver Thomas <oliver.thomas@bristol.ac.uk>
2018-07-03 20:35:29 +0100

    merge

--- ubuntuinstall.sh
+++ ubuntuinstall.sh
@@ -4,6 +4,7 @@ sudo apt-get update
 sudo apt-get install -y build-essential
 
 sudo apt-get install -y  python3 python3-dev python3-setuptools python-pip pandoc python-setuptools indicator-kdeconnect kdeconnect libsymengine-dev python-symengine python3-numpy python3-matplotlib python3-scipy python3-nose ipython3 python3-pip python3-sympy python3-tk vim-gtk okular texlive-full biber screenfetch
+sudo -H python3 -m pip install wheel
 sudo -H pip3 install --user --upgrade projectq
 #sudo pip install jupyter
 #Vim theme paste with "+p 
