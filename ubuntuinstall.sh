7f8a651b5c9c05221617871010665ee4a4dfb8c5
Oliver Thomas <oliver.thomas@bristol.ac.uk>
2018-07-06 22:23:14 +0100

    fixed merge problems

--- ubuntuinstall.sh
+++ ubuntuinstall.sh
@@ -1,13 +1,20 @@
 #sudo add-apt-repository ppa:symengine/ppa 
 #sudo add-apt-repository ppa:webupd8team/indicator-kdeconnect 
 sudo apt-get update
-<<<<<<< HEAD
+
+sudo apt-get install -y build-essential
+
 sudo apt-get install -y python-pip pandoc python-setuptools libsymengine-dev python-symengine python3-numpy python3-matplotlib python3-scipy python3-nose ipython3 python3-pip python3-sympy 
 
 sudo apt-get install -y vim-gtk okular texlive-full biber screenfetch
 sudo apt-get install -y indicator-kdeconnect kdeconnect 
 
-sudo add-apt-repository universe
+
+sudo apt-get install -y  python3 python3-dev python3-setuptools python-pip pandoc python-setuptools indicator-kdeconnect kdeconnect libsymengine-dev python-symengine python3-numpy python3-matplotlib python3-scipy python3-nose ipython3 python3-pip python3-sympy python3-tk vim-gtk okular texlive-full biber screenfetch
+python3 -m pip install wheel
+python3 -m pip install --user projectq
+
+#sudo add-apt-repository universe
 #sudo apt install python3-proselint
 
 sudo apt-get install python3-colorama python3-click python3-future 
@@ -19,13 +26,7 @@ sudo apt-get install npm
 sudo npm install -g write-good
 sudo npm install --global prettier
 
-=======
-sudo apt-get install -y build-essential
 
-sudo apt-get install -y  python3 python3-dev python3-setuptools python-pip pandoc python-setuptools indicator-kdeconnect kdeconnect libsymengine-dev python-symengine python3-numpy python3-matplotlib python3-scipy python3-nose ipython3 python3-pip python3-sympy python3-tk vim-gtk okular texlive-full biber screenfetch
-python3 -m pip install wheel
-python3 -m pip install --user projectq
->>>>>>> 9afc6b2fcf89a879876c9e9dada57a1b38edf61b
 #sudo pip install jupyter
 
 #Vim theme paste with "+p 
