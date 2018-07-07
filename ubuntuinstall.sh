4d40db9da155fe27e52ad9bfa696131d1842c692
Oliver <oliver.thomas404@gmail.com>
2018-07-07 17:32:01 +0100

    sorted ubuntuinstall

--- ubuntuinstall.sh
+++ ubuntuinstall.sh
@@ -1,33 +1,42 @@
-#sudo add-apt-repository ppa:symengine/ppa 
-#sudo add-apt-repository ppa:webupd8team/indicator-kdeconnect 
+sudo add-apt-repository ppa:symengine/ppa 
+sudo add-apt-repository ppa:webupd8team/indicator-kdeconnect 
+
 sudo apt-get update
 
 sudo apt-get install -y build-essential
 
-sudo apt-get install -y python-pip pandoc python-setuptools libsymengine-dev python-symengine python3-numpy python3-matplotlib python3-scipy python3-nose ipython3 python3-pip python3-sympy 
-
+#ubuntu stuff
 sudo apt-get install -y vim-gtk okular texlive-full biber screenfetch
 sudo apt-get install -y indicator-kdeconnect kdeconnect 
 
+#python3 stuff
+sudo apt-get install -y python3 python3-dev python3-setuptools python3-pip python3-tk
+sudo apt-get install -y python3-numpy python3-matplotlib python3-scipy python3-nose 
 
-sudo apt-get install -y  python3 python3-dev python3-setuptools python-pip pandoc python-setuptools indicator-kdeconnect kdeconnect libsymengine-dev python-symengine python3-numpy python3-matplotlib python3-scipy python3-nose ipython3 python3-pip python3-sympy python3-tk vim-gtk okular texlive-full biber screenfetch
+#sympy 
+sudo apt-get install -y libsymengine-dev python-symengine python3-sympy
+sudo apt-get install -y ipython3 pandoc python3-pip
+
+#pip 
 python3 -m pip install wheel
 python3 -m pip install --user projectq
+
+#for notebooks
+#python3 -m pip install jupyter
 
 #sudo add-apt-repository universe
 #sudo apt install python3-proselint
 
+#python linters
 sudo apt-get install python3-colorama python3-click python3-future 
 sudo dpkg -i python3-proselint_0.7.0-1_all.deb
 sudo apt-get install flake8 
 sudo apt-get install -y autopep8
 
+#text linters
 sudo apt-get install npm
 sudo npm install -g write-good
 sudo npm install --global prettier
-
-
-#sudo pip install jupyter
 
 #Vim theme paste with "+p 
 #sudo wget "https://raw.githubusercontent.com/TroyFletcher/vim-colors-synthwave/master/colors/synthwave.vim" -P ~/.vim/colors/
