f5402081e3b3df658a87b40a287a28ff0d2c651d
Oliver <oliver.thomas404@gmail.com>
2018-08-31 20:11:03 +0100

    updated for mint v19, smpy repo is broken in ubuntu bionic

--- ubuntuinstall.sh
+++ ubuntuinstall.sh
@@ -1,13 +1,14 @@
-#sudo add-apt-repository ppa:symengine/ppa 
-#sudo add-apt-repository ppa:webupd8team/indicator-kdeconnect 
-#sudo add-apt-repository ppa:jonathonf/vim
+sudo add-apt-repository ppa:symengine/ppa 
+sudo add-apt-repository ppa:webupd8team/indicator-kdeconnect 
+sudo add-apt-repository ppa:jonathonf/vim
 
 sudo apt-get update
 
 sudo apt-get install -y build-essential
 
 #ubuntu stuff
-sudo apt-get install -y vim-gtk okular texlive biber screenfetch
+sudo apt-get install -y vim
+sudo apt-get install -y okular texlive biber screenfetch
 sudo apt-get install -y indicator-kdeconnect kdeconnect 
 
 #python3 stuff
@@ -36,24 +37,24 @@ python3 -m pip install isort
 sudo apt install python3-proselint
 
 #python linters
-sudo apt-get install python3-colorama python3-click python3-future 
+sudo apt-get install -y python3-colorama python3-click python3-future 
 #sudo dpkg -i python3-proselint_0.7.0-1_all.deb
-sudo apt-get install flake8 
+sudo apt-get install -y flake8 
 #sudo apt-get install -y autopep8
 #sudo apt-get install -y pylint pyflakes
 
 
 #text linters
-sudo apt-get install npm
+sudo apt-get install -y npm
 sudo npm install -g write-good
 sudo npm install --global prettier
 
 #Vim theme paste with "+p 
-#wget "https://raw.githubusercontent.com/TroyFletcher/vim-colors-synthwave/master/colors/synthwave.vim" -P ~/.vim/colors/
+wget "https://raw.githubusercontent.com/TroyFletcher/vim-colors-synthwave/master/colors/synthwave.vim" -P ~/.vim/colors/
 
 #git clone https://github.com/vim/vim.git
 #cd ./vim/src/
 #make 
 #sudo make install 
 
-#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
